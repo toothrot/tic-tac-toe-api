13b5f1626d785ce44acab0546e3cfdacb109fa6f
toothrot <arakoczy@gmail.com>
2012-03-16 16:34:04 -0700

    testing

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source "http://rubygems.org"
 gem 'sinatra'
+gem 'yajl-ruby'
+gem 'rake'
---
+++ test/test_first.rb
@@ -0,0 +1,19 @@
+require File.join(File.absolute_path(File.dirname(__FILE__)),'..','app')
+require 'test/unit'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class HelloWorldTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_says_hello_world
+    get '/'
+    assert last_response.ok?
+    assert_equal 'Howdy', last_response.body
+  end
+end
